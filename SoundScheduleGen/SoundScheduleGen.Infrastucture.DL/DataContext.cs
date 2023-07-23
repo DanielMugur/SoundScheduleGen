533152e2742f31528e53af23b1dfc6d365dab916
Daniel Mugur <daniel.swe97@gmail.com>
2023-07-23 16:34:47 +0200

    Creación de código para la conexión con DB por medio de EF

---
+++ SoundScheduleGen/SoundScheduleGen.Infrastucture.DL/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoundScheduleGen.Domain.DL;
+
+namespace SoundScheduleGen.Infrastucture.DL
+{
+    public class DataContext : DbContext
+    {
+        #region Constructors
+        public DataContext()
+        {
+            
+        }
+
+        #endregion
+
+        #region DbSetMembers
+
+        public DbSet<PersonHermano> PersonHermano { get; set; }
+
+
+        #endregion
+    }
+}
