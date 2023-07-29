669c6a59cea4b74abfba52398782f82288ec2b0c
Daniel Mugur <daniel.swe97@gmail.com>
2023-07-29 02:08:40 +0200

    MappingProfiles class and DbContext Config

--- SoundScheduleGen/SoundScheduleGen.Infrastucture.DL/DataContext.cs
+++ SoundScheduleGen/SoundScheduleGen.Infrastucture.DL/DataContext.cs
@@ -1,10 +1,33 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SoundScheduleGen.Domain.DL;
+using SoundScheduleGen.Domain.DL.Person;
 
 namespace SoundScheduleGen.Infrastucture.DL
 {
     public class DataContext : DbContext
     {
+        #region Constants and Variables
+
+        public DbContext DbContext { get; }
+        private readonly string _connectionString;
+
+        #endregion
+
+        #region DbConfig
+
+        public DataContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer(_connectionString);
+        }
+
+        #endregion
+
+
         #region Constructors
         public DataContext()
         {
